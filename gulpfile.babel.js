26e1f6b3387690158a852d4687c72b71cdd6d862
Илья Оловянников <ilaolovannikov@iMac-Ila.local>
2021-05-27 17:58:48 +0300

    add files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,13 @@
 'use strict';
 
-import gulp         from 'gulp';
-import babel        from 'gulp-babel';
-import imagemin     from 'gulp-imagemin';
-import mjml         from 'gulp-mjml';
-import newer        from 'gulp-newer';
-import browserSync  from 'browser-sync';
-import del          from 'del';
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const imagemin = require('gulp-imagemin');
+const mjml = require('gulp-mjml');
+const newer = require('gulp-newer');
+const browserSync = require('browser-sync');
+const del = require('del');
+
 
 
 // Simple config: just set up paths for everything
@@ -31,7 +32,7 @@ const paths = {
 // Transform the MJML files using `gulp-mjml` into HTML files
 // Waiting for the process to be `done` before reloading the browsers
 //
-export const generateHtml = (done) => {
+const generateHtml = (done) => {
   gulp.src(paths.html.src)
     .pipe(mjml())
     .pipe(gulp.dest(basePaths.dist));
@@ -45,7 +46,7 @@ export const generateHtml = (done) => {
 // Get all the new images (gulp-newer), compress them through imagemin,
 // throw them in the `dist` folder & then inject them with BrowserSync
 //
-export const images = (done) => {
+const images = (done) => {
   gulp.src(paths.images.src)
     .pipe(newer(paths.images.dist))
     .pipe(imagemin({
@@ -62,7 +63,7 @@ export const images = (done) => {
 // Start a BrowserSync server on the `dist` folder which will listen
 // to the changes on html & images generated
 //
-export const watch = () => {
+const watch = () => {
   browserSync.init({
     server: basePaths.dist
   })
